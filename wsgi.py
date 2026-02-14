85081abb0c0a4be6280ffd0f55290936804cb92c
rotss2 <110035339+rotss2@users.noreply.github.com>
2026-02-14 21:15:43 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
-from app import create_app
+from app import create_app  # Import your Flask app from the app module
 
+# Create an instance of the app
 app = create_app()
+
+if __name__ == "__main__":
+    app.run()  # Run the app
